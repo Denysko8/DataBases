880df974a533299d9f35ff5f2f3d875b3268dafb
Denysko8 <denisnesterovic2@gmail.com>
2025-10-12 22:58:36 +0300

    Swagger added

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from flask import Flask
+from flasgger import Swagger
 from db_init import db
 from my_project.auth.route.airport_route import airport_bp
 from my_project.auth.route.plane_route import plane_bp
@@ -13,6 +14,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.environ.get('USER'
 
 
 db.init_app(app)
+
+swagger = Swagger(app)
 
 app.register_blueprint(airport_bp, url_prefix='/api')
 app.register_blueprint(plane_bp, url_prefix='/api')
