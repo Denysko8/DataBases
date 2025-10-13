1ea9252b8d23dd4a93a0a6e83881e2f5a4d9d917
Denysko8 <denisnesterovic2@gmail.com>
2025-10-13 13:24:47 +0300

    so now we're on master

--- app.py
+++ app.py
@@ -1,18 +1,46 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from db_init import db
 from my_project.auth.route.airport_route import airport_bp
 from my_project.auth.route.plane_route import plane_bp
 from my_project.auth.route.flight_route import flight_bp
+from flask import Flask, jsonify
+from flasgger import Swagger
 
 app = Flask(__name__)
+Swagger(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:denysko@localhost/mydb'
+
+load_dotenv()
+app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.environ.get('USER')}:{os.environ.get('PASSWORD')}@{os.environ.get('PUBLIC_IP')}:3306/{os.environ.get('DB_NAME')}"
+
 
 db.init_app(app)
 
 app.register_blueprint(airport_bp, url_prefix='/api')
 app.register_blueprint(plane_bp, url_prefix='/api')
 app.register_blueprint(flight_bp, url_prefix='/api')
+
+
+@app.route('/api/health', methods=['GET'])
+def health_check():
+    """
+    Health check
+    ---
+    tags:
+      - Health
+    responses:
+      200:
+        description: OK
+        schema:
+          type: object
+          properties:
+            status:
+              type: string
+              example: healthy
+    """
+    return jsonify({"status": "healthy"})
 
 if __name__ == '__main__':
     app.run(debug=True)
