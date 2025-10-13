42a5663be10663cfc3bca3669a4a6a51d36b6c9a
Denysko8 <denisnesterovic2@gmail.com>
2025-10-13 13:31:15 +0300

    lets try one route

--- my_project/auth/route/airport_route.py
+++ my_project/auth/route/airport_route.py
@@ -24,118 +24,19 @@ def get_all_airports():
 
 @airport_bp.route('/airports/<int:airport_id>', methods=['GET'])
 def get_airport_by_id(airport_id):
-        """
-        Get airport by ID
-        ---
-        tags:
-            - Airports
-        parameters:
-            - name: airport_id
-                in: path
-                type: integer
-                required: true
-                description: ID of the airport
-        responses:
-            200:
-                description: Airport object
-                schema:
-                    type: object
-            404:
-                description: Airport not found
-        """
         return AirportController.get_airport_by_id(airport_id)
 
 
 @airport_bp.route('/airports', methods=['POST'])
 def create_airport():
-        """
-        Create airport
-        ---
-        tags:
-            - Airports
-        consumes:
-            - application/json
-        parameters:
-            - in: body
-                name: body
-                required: true
-                schema:
-                    type: object
-                    properties:
-                        name:
-                            type: string
-                        city:
-                            type: string
-                        country:
-                            type: string
-                        iata_code:
-                            type: string
-        responses:
-            201:
-                description: Created
-                schema:
-                    type: object
-            400:
-                description: Invalid input
-        """
         return AirportController.create_airport()
 
 
 @airport_bp.route('/airports/<int:airport_id>', methods=['PUT'])
 def update_airport(airport_id):
-        """
-        Update airport
-        ---
-        tags:
-            - Airports
-        parameters:
-            - name: airport_id
-                in: path
-                type: integer
-                required: true
-                description: ID of the airport
-            - in: body
-                name: body
-                required: true
-                schema:
-                    type: object
-                    properties:
-                        name:
-                            type: string
-                        city:
-                            type: string
-                        country:
-                            type: string
-                        iata_code:
-                            type: string
-        responses:
-            200:
-                description: Updated
-            400:
-                description: Invalid input
-            404:
-                description: Airport not found
-        """
         return AirportController.update_airport(airport_id)
 
 
 @airport_bp.route('/airports/<int:airport_id>', methods=['DELETE'])
 def delete_airport(airport_id):
-        """
-        Delete airport
-        ---
-        tags:
-            - Airports
-        parameters:
-            - name: airport_id
-                in: path
-                type: integer
-                required: true
-                description: ID of the airport to delete
-        responses:
-            204:
-                description: Deleted
-            404:
-                description: Airport not found
-        """
         return AirportController.delete_airport(airport_id)
